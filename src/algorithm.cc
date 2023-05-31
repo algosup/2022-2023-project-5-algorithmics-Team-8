3486458709442dd9dd6eb28b3dd224de8dd448aa
Louis-de-Lavenne-de-Choulot <louis.delavenne@algosup.com>
2023-05-31 12:05:36 +0200

    "Fix" "parsing"
    Co-Authored-By: Malo-Archimbaud <97161471+Malo-Archimbaud@users.noreply.github.com>

--- src/algorithm.cc
+++ src/algorithm.cc
@@ -5,6 +5,7 @@
 #include <algorithm>
 #include <iterator>
 #include <map>
+#include <fstream>
 #include <iomanip>
 #include <cmath>
 #include "include/tank.h"
@@ -50,46 +51,93 @@ void Solve(Formula F, double a)
   }
 }
 
-int main()
+//get args
+int main(int argc, char *argv[])
 {
-  int n;
-  std::cin >> n;
+  std::string path = std::string(argv[1]);
+  std::cout << path << std::endl;
+
+  
+  FILE *file = fopen(path.c_str(), "r");
+
+  std::map<std::string, Tank *> *tanks_map = new std::map<std::string, Tank *>();
+  std::map<std::string, double> *formula = new std::map<std::string, double>();
+  double *total = new double();
+
+  if (file == NULL)
+  {
+    std::cout << "Error opening file" << std::endl;
+    exit(1);
+  }
+  else
+  {
+    std::cout << "RUN" << std::endl;
+    char line[256];
+    while (fgets(line, sizeof(line), file))
+    {
+        // print line
+        // std::cout << line << std::endl;
+        readline(line,*tanks_map,*formula,*total);
+      
+    }
+    std::cout << "END RUN" << std::endl;
+  }
+
+  // tanks = std::vector<Tank>(tanks_map->size());
+  for (auto it = tanks_map->begin(); it != tanks_map->end(); ++it)
+  {
+    tanks.push_back(*it->second);
+  }
+
+  // formulas = std::vector<Formula>(formula->size());
+  for (auto it = formula->begin(); it != formula->end(); ++it)
+  {
+    Formula f;
+    f.name = it->first;
+    f.output = it->second;
+    formulas.push_back(f);
+  }
+
+
+
+  int n = formulas.size();
   for (int i = 0; i < n; i++)
   {
     Formula f;
-    std::cin >> f.name;
-    std::cin >> f.quantity;
-    int m;
-    std::cin >> m;
+    f.name = formulas[i].name;
+    f.quantity = formulas[i].quantity;
+    // int m = tanks.size(); this is false but since wtf then let's assume it is
+    int m = tanks.size();
     for (int j = 0; j < m; j++)
     {
-      std::string input;
-      double input_quantity;
-      std::cin >> input;
-      std::cin >> input_quantity;
+      std::string input = tanks[j].formula.begin()->first;
+      double input_quantity = tanks[j].formula.begin()->second;
+      // std::cin >> input;
+      //! WTF std::cin >> input_quantity;
       f.inputs.push_back(input);
       f.inputs_quantity.push_back(input_quantity);
     }
-    std::cin >> f.output_quantity;
-    std::cin >> f.output;
+    f.output_quantity = formulas[i].output_quantity;
+    f.output = formulas[i].output;
     f.is_solved = false;
     formulas.push_back(f);
   }
-  int t;
-  std::cin >> t;
+  int t = tanks.size();
   for (int i = 0; i < t; i++)
   {
     Tank tank(0,"/");
-    std::cin >> tank.capacity;
+    tank.capacity = tanks[i].capacity;
     tank.formula["/"] = 100;
     tanks.push_back(tank);
   }
-  double a;
-  std::cin >> a;
+  double a = *total; // ????????
   Solve(formulas[0], a);
   for (int i = 0; i < tanks.size(); i++)
   {
-    std::cout << tanks[i].capacity << " " << tanks[i].formula["/"] << std::endl;
+    // std::cout << tanks[i].capacity << " " << tanks[i].formula.begin()->first << " " << tanks[i].formula.begin()->second << std::endl; formatted to get lisible strings and numbers formated to 2 decimals
+    std::cout << std::fixed << std::setprecision(2) << tanks[i].capacity << " " << tanks[i].formula.begin()->first << " " << tanks[i].formula.begin()->second << std::endl;
   }
+
   return 0;
 }
+
--- src/sebastien.cc
+++ src/sebastien.cc
@@ -3,71 +3,91 @@
 #include <vector>
 #include <string>
 
-
-
 /**
  * reads the content of a file into a Result struct
- * @param filepath Relative path of the file to 
+ * @param filepath Relative path of the file to
  */
 
 Result configparser(std::string filepath)
 {
-  std::string line;
-  std::ifstream file;
-  file.open(filepath);
-  
-  std::map<std::string,Tank*> *tanks = new std::map<std::string,Tank*>();
-  std::map<std::string,double> *formula = new std::map<std::string,double>();
-  double *total = new double();
-  
-  while(getline(file,line))
+
+  FILE *file = fopen(filepath.c_str(), "r");
+
+  // std::map<std::string, Tank *> *tanks = new std::map<std::string, Tank *>();
+  // std::map<std::string, double> *formula = new std::map<std::string, double>();
+  // double *total = new double();
+
+  if (file == NULL)
   {
-       readline(line,*tanks,*formula,*total);
+    std::cout << "Error opening file" << std::endl;
+    exit(1);
   }
-  
-  Result result = {*tanks,*formula};
+  else
+  {
+    char line[256];
+    while (fgets(line, sizeof(line), file))
+    {
+      // print line
+      std::cout << line << std::endl;
+      // readline(line,*tanks,*formula,*total);
+    }
+  }
+
+  Result result = {};
   return result;
 }
-
 
 // Comment => line starts with '!'
 // Total => single value on line
 // Formula => 2 values on line
 // Tank => 3 values on line
-void readline(std::string line, std::map<std::string,Tank*>& tanks, std::map<std::string,double>& formula,double& total)
+void readline(std::string line, std::map<std::string, Tank *> &tanks, std::map<std::string, double> &formula, double &total)
 {
   std::string separator = ";";
   size_t pos = 0;
   std::string token;
   std::vector<std::string> tokens;
-  while ((pos = line.find(separator)) != std::string::npos)
+
+  // if line does not contain separator check if it is a comment
+  if (line[0] == '!' || line == "\n")
   {
-    token = line.substr(0,pos);
-    std::vector<std::string> values;
-    if(token == "!") return;
+
+    std::cout << line << std::endl;
+    std::cout << "Comment or empty line: " << std::endl;
+    return;
   }
-  switch (token.length())
+
+  // while  not at eof
+  printf("pos: %d\n", pos);
+  token = line.substr(0, line.length());
+  printf("token: %s\n", token.c_str());
+  std::vector<std::string> values;
+  switch (token[0])
   {
-      case 1:
-      {
-        total = std::stod(tokens[0]);
-        break;
-      }
-      case 2:
-      {
-        formula[tokens[0]] =std::stod(tokens[1].substr(0,tokens[1].length()-1));
-        break;
-      }
-      case 3:
-      {
-        Tank *tank = new Tank(std::stod(tokens[1]),tokens[2] == "/" ? "" : tokens[2]);
-        tanks[tokens[0]] = tank;
-        break;
-      }
-      default:
-        throw;
-        break;
+  case 48 ... 57:
+  {
+    total = std::stod(token);
+    break;
   }
-    
-  
+  case 65 ... 122:
+  {
+    formula[token.substr(0, token.find(separator))] = std::stod(token.substr(token.find(separator) + 1, token.length()));
+    // formula[tokens[0]] = std::stod(tokens[1].substr(0, tokens[1].length() - 1));
+    break;
+  }
+  case '#':
+  {
+    // std::cout << "Comment: " << token << std::endl;
+    // std::cout << std::stod(token.substr(token.find(separator) + 1, token.find_last_of(separator) + 1)) << std::endl;
+    // std::cout << token.substr(token.find_last_of(separator) + 1, token.length()) << std::endl;
+
+    Tank *tank = new Tank(std::stod(token.substr(token.find(separator) + 1, token.length())), token.substr(token.find_last_of(separator) + 1, token.length()));
+    tanks[token.substr(1, token.find(separator))] = tank;
+    break;
+  }
+  default:
+    throw;
+    break;
+  }
+  line.erase(0, pos + separator.length());
 }
